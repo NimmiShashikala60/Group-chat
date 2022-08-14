e1f900a26f7ded9faccde31fac79fe5a98ee15cb
NimmiShashikala60 <nimmishashikala385@gmail.com>
2022-08-14 23:26:11 +0530

    new update

---
+++ src/Controller/LoginForm.java
@@ -0,0 +1,11 @@
+package View;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class LoginForm {
+    public TextField txtUserName;
+    public Button btnLogIn;
+    public ImageView imgUser;
+}
